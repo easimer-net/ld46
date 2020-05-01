52e5b134ff7843e54abd8abf65b7cc30ef48f36d
Daniel Meszaros <easimer@gmail.com>
2020-05-01 23:26:33 +0200

    Test table var_names and fields

--- entity_gen/tests_parser.cpp
+++ entity_gen/tests_parser.cpp
@@ -22,7 +22,7 @@ TEST_CASE("Empty table", "[parser]") {
     };
 
     REQUIRE(SyntaxCheckTop(tokens));
-    auto tables = ParseTop(tokens);
+    auto const tables = ParseTop(tokens);
     REQUIRE(tables.size() == 1);
     auto const& table = tables[0];
     REQUIRE(table.name == "Test");
@@ -30,4 +30,51 @@ TEST_CASE("Empty table", "[parser]") {
     REQUIRE(table.fields.size() == 0);
     REQUIRE(table.constants.size() == 0);
     REQUIRE(table.flags == k_unFieldFlags_None);
+}
+
+TEST_CASE("Empty table with custom var_name", "[parser]") {
+    Vector<Token> const tokens = {
+        TOK(Table), TOKU("Test"), TOKU("ents"), TOK(Curly_Open),
+        TOK(Curly_Close),
+    };
+
+    REQUIRE(SyntaxCheckTop(tokens));
+    auto const tables = ParseTop(tokens);
+    REQUIRE(tables.size() == 1);
+    auto const& table = tables[0];
+    REQUIRE(table.name == "Test");
+    REQUIRE(table.var_name == "ents");
+    REQUIRE(table.fields.size() == 0);
+    REQUIRE(table.constants.size() == 0);
+    REQUIRE(table.flags == k_unFieldFlags_None);
+}
+
+TEST_CASE("Scalar fields", "[parser]") {
+    Vector<Token> const tokens = {
+        TOK(Table), TOKU("Test"), TOK(Curly_Open),
+            TOKU("field1"), TOK(Colon), TOKU("int"), TOK(Semicolon),
+            TOKU("field2"), TOK(Colon), TOKU("float"), TOK(Semicolon),
+        TOK(Curly_Close),
+    };
+
+    REQUIRE(SyntaxCheckTop(tokens));
+    auto const tables = ParseTop(tokens);
+    REQUIRE(tables.size() == 1);
+    auto const& table = tables[0];
+    REQUIRE(table.name == "Test");
+    REQUIRE(table.var_name == "tests");
+    REQUIRE(table.fields.size() == 2);
+    REQUIRE(table.constants.size() == 0);
+    REQUIRE(table.flags == k_unTableFlags_None);
+
+    auto const& field1 = table.fields[0];
+    REQUIRE(field1.name == "field1");
+    REQUIRE(field1.type.base == "int");
+    REQUIRE(field1.type.count == 1);
+    REQUIRE(field1.flags == k_unFieldFlags_None);
+    auto const& field2 = table.fields[1];
+    REQUIRE(field2.name == "field2");
+    REQUIRE(field2.type.base == "float");
+    REQUIRE(field2.type.count == 1);
+    REQUIRE(field2.flags == k_unFieldFlags_None);
 }
